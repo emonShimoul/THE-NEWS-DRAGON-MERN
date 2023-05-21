000c0dba5dde0f3ea07e5123d8d9e588f900d95a
emonShimoul <emonshimoul@gmail.com>
2023-05-21 18:22:54 +0600

    load news category data from server and style news card component and react ratings

--- the-news-dragon-client/src/pages/Home/Category/Category.jsx
+++ the-news-dragon-client/src/pages/Home/Category/Category.jsx
@@ -1,10 +1,17 @@
-import { useParams } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import { useLoaderData, useParams } from "react-router-dom";
+import NewsCard from "../NewsCard/NewsCard";
 
 const Category = () => {
   let { id } = useParams();
+  const categoryNews = useLoaderData();
+
   return (
     <div>
-      <h2>Category: {id}</h2>
+      <h2>Category News: {categoryNews.length}</h2>
+      {categoryNews.map((news) => (
+        <NewsCard key={news._id} news={news}></NewsCard>
+      ))}
     </div>
   );
 };
